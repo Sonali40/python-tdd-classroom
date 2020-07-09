9f196b123104d49270530122f67b2e337682279f
Sonali50 <cloud_user@mylabserver.com>
2020-07-09 18:16:45 +0530

    string file modified

--- src/string_exercise.py
+++ src/string_exercise.py
@@ -3,17 +3,27 @@ class StringExercise:
     def __init__(self):
         pass   # Do some initial setup in this constructor method, if needed
 
-    def reverse_string(self, input_str):
+    def reverse(s): 
+  str = "" 
+  for i in s: 
+    str = i + str
+  return str
+
         """
         Reverses order of characters in string input_str.
         """
         return None
 
-    def is_english_vowel(self, character):
+    def is_english_vowel(self, char):
         """
         Returns True if character is an english vowel
         and False otherwise.
         """
+        
+    all_vowels = 'aeiou'
+    return char in all_vowels
+    print(is_vowel('c'))
+    print(is_vowel('e'))
         return None
 
     def find_longest_word(self, sentence):
@@ -21,11 +31,23 @@ class StringExercise:
         Returns the longest word in string sentence.
         In case there are several, return the first.
         """
-        return None
+        length = 0
+      
+    for word in sentence.split(): 
+        if(len(word) > length): 
+            length = len(word) 
+      
+    return length  
+      
 
     def get_word_lengths(self, text):
         """
         Returns a list of integers representing
         the word lengths in string text.
         """
-        return None
+         wordlen = []
+    for n in text:
+        wordlen.append((len(n), n))
+    wordlen.sort()
+    return wordlen[-1][1]
+     
